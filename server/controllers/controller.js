8d71c261b7429a0427ddd1164a11b8e86fee8268
zanadh <fadzim018@gmail.com>
2020-09-28 19:29:32 +0700

    edit delete method

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -96,15 +96,16 @@ class Controller {
             })
     }
     static deleteTodo(req,res){
-        Todo.destroy(req.params.id)
+        Todo.destroy({where:{id:req.params.id}})
             .then(result=>{
                 if(result){
-                    res.status(200).json({"message":"Internal Server Error"})
+                    res.status(200).json({"message":"Todo success to delete"})
                 }else{
                     res.status(404).json({"message":"Todo Not Found"})
                 }
             })
             .catch(err=>{
+                console.log(err);
                 res.status(500).json({"message":"Internal Server Error"})
             })
     } 
