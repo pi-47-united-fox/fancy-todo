9031993d0adff796f9e7584f43cfea353b4ea25b
midham14 <mohammadidham14@gmail.com>
2020-09-29 01:33:19 +0700

    add findbyid,put,patch,delete

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const TodoController = require('../controllers')
 
 router.get('/todos',TodoController.getTodoHandler)
 router.post('/todos',TodoController.createTodoHandler)
+router.get('/todos/:id',TodoController.getTodoByIdHandler)
+router.put('/todos/:id',TodoController.putTodoHandler)
+router.patch('/todos/:id', TodoController.patchTodoHandler)
+router.delete('/todos/:id',TodoController.deleteTodoHandler)
 
 
 module.exports = router
