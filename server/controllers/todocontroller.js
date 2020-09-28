60a3fe492b0628c5f2a2d803a57b3439befc8ced
aldamtrisena <aldam3sena@gmail.com>
2020-09-28 13:54:15 +0700

    testpostman

--- server/controllers/todocontroller.js
+++ server/controllers/todocontroller.js
@@ -1,21 +1,33 @@
 const { Todo } = require("../models")
 
 class TodoController {
+    // get/todos
     static getDataTodo(req, res) {
+        Todo.findAll()
+            .then(result => {
 
+            })
     }
+    // post/todos
     static postInputTodo(req, res) {
 
+        res.send("todospost")
     }
+    //Get /todos/:id
     static findTodoById(req, res) {
-
+        res.send("todosgetid")
     }
+    //Put /todos/:id
     static updateTodoById(req, res) {
 
     }
+
+    //Patch /todos/:id
     static changeStatusTodo(req, res) {
 
     }
+
+    //DELETE /todos/:id
     static deleteTodoById(req, res) {
 
     }
