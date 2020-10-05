47b4f1410bee3b935db0dcd126dfcb4fbdd66041
Samuel Aditia <samueladitia95@gmail.com>
2020-10-05 17:47:37 +0700

    Server ready to deploy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 "use strict";
 
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+	require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
