a519072e29249c519b217083eed9b5da19972764
enricoanto <enricoanto@yahoo.com>
2020-10-06 07:17:13 +0700

    revisi

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const router = require('./routes/index.js')
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
-console.log(new Date( new Date().toUTCString() ).toLocaleString())
+
 app.use(router)
 app.listen(port,()=> {
     console.log(`App listening at http://localhost:${port}`)
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router()
 const TodoController = require('../controllers/Todo.js')
 const UserController = require('../controllers/User.js')
-const ApiController = require('../controllers/Api.js')
+//const ApiController = require('../controllers/Api.js')
 const {authentication, authorization} = require('../middlewares/authentication-authorization.js')
 
 router.get('/', (req, res)=>{
@@ -10,8 +10,8 @@ router.get('/', (req, res)=>{
 })
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
-router.get ('/music',ApiController.searchMusic)
-router.get ('/earthquake',ApiController.earthquake)
+//router.get ('/music',ApiController.searchMusic)
+//router.get ('/earthquake',ApiController.earthquake)
 router.use(authentication)
 router.get('/todos', TodoController.findAllTodo)
 router.get('/todos/:id', TodoController.findByIdTodo)
